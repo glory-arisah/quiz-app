f4f11a45b9f91fd9084426309b8efdb884746dbe
Glory Arisah <garisah@ipnxnigeria.net>
2023-06-26 17:08:08 +0100

    configuration: vue router and pinia store initial setup

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+const store = defineStore({
+  id: "store",
+  state: {},
+  actions: {},
+  getters: {},
+});
+
+export default store;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Home component</div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
